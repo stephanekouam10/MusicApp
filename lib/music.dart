8f4023c4dc6bd842b31a49bc60b6d31d5b7a020b
stephanekouam10 <stephane.kouam237@gmail.com>
2020-11-24 09:41:23 +0100

    the first commit

---
+++ lib/music.dart
@@ -0,0 +1,15 @@
+class Musique {
+
+  String titre;
+  String auteur;
+  String imagePath;
+  String musicUrl;
+
+  Musique(String titre, String auteur, String imagePath, String musicUrl) {
+    this.titre = titre;
+    this.auteur = auteur;
+    this.imagePath = imagePath;
+    this.musicUrl = musicUrl;
+  }
+
+}
